41d99bf13a55277824a05d2040168d39f01153f6
Yana Zhdanova <zhdanova.yana2003@yandex.ru>
2023-09-25 16:44:15 +0300

    POST-request

--- bmstu_lab/app/models.py
+++ bmstu_lab/app/models.py
@@ -8,6 +8,75 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class BankServices(models.Model):
     bank_service_id = models.AutoField(primary_key=True)
     title = models.CharField(blank=True, null=True)
@@ -23,6 +92,51 @@ class BankServices(models.Model):
         db_table = 'bank_services'
 
 
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Requests(models.Model):
     request_id = models.AutoField(primary_key=True)
     request_status = models.TextField(blank=True, null=True)  # This field type is a guess.
@@ -30,6 +144,7 @@ class Requests(models.Model):
     formation_date = models.DateField(blank=True, null=True)
     completion_date = models.DateField(blank=True, null=True)
     user = models.ForeignKey('Users', models.DO_NOTHING, blank=True, null=True)
+    admin = models.ForeignKey('Users', models.DO_NOTHING, related_name='requests_admin_set', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -37,13 +152,13 @@ class Requests(models.Model):
 
 
 class RequestsServices(models.Model):
-    request_service = models.OneToOneField(Requests, models.DO_NOTHING, primary_key=True)
-    bank_service = models.ForeignKey(BankServices, models.DO_NOTHING, blank=True, null=True)
-    request_id = models.IntegerField(blank=True, null=True)
+    bank_service = models.OneToOneField(BankServices, models.DO_NOTHING, primary_key=True)  # The composite primary key (bank_service_id, request_id) found, that is not supported. The first column is selected.
+    request = models.ForeignKey(Requests, models.DO_NOTHING)
 
     class Meta:
         managed = False
         db_table = 'requests_services'
+        unique_together = (('bank_service', 'request'),)
 
 
 class Users(models.Model):
@@ -52,6 +167,7 @@ class Users(models.Model):
     surname = models.CharField(blank=True, null=True)
     password = models.CharField(blank=True, null=True)
     phone_number = models.CharField(blank=True, null=True)
+    admin_flag = models.BooleanField(blank=True, null=True)
 
     class Meta:
         managed = False
